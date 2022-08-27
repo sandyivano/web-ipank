41183236ce6a7005dddfb827c830859ae88e4a76
sandyivano <masyarakatsampah337@gmail.com>
2022-08-27 21:13:19 +0845

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '8px',
+
+    },
+    extend: {
+      colors: {
+        primary: '#84cc16',
+        secondary: '#64748b',
+        dark: '#0F172A',
+      },
+      screens: {
+        '2xl': '1000px',
+      },
+    },
+  },
+  plugins: [],
+};
